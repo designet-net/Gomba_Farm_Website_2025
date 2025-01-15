13eab95bb32f21ea81eb3e1c59d006a98a1c5db4
designet-net <noahjjo@gmail.com>
2025-01-15 15:11:12 +0300

    Initial commit

--- who_section.php
+++ who_section.php
@@ -23,5 +23,25 @@
                 </p>
             </div>
         </div>
+        <div class="two-column-row">
+            <div class="column">
+                <h1 style="color:green">Upcoming Events</h1>
+                <h3>
+                    Explore and Learn: Vocational Tour to Gomba Next Generation Farm
+                </h3>
+                <p>
+                    Join us on Thursday, the eighteenth of January, for an enlightening vocational tour to Gomba Next Generation Farm. This event promises an exciting opportunity to gain practical knowledge and hands-on experience in modern farming techniques.
+                </p>
+                <p>
+                    Whether you're a student, an aspiring farmer, or an agriculture enthusiast, this tour is designed to inspire and equip you with skills for sustainable growth. Don’t miss this chance to witness agricultural excellence in action!
+                </p>
+                <p>
+                    Be part of this transformative experience and step into the future of farming!
+                </p>
+                <div class="box-content">
+                    <img src="images/gomb-1.jpg" alt="next25" style="width: 100%; height: auto; border-radius: 8px;" />
+                </div>
+            </div>
+        </div>
     </div>    
 </section>
